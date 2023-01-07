83dceb6c87534781d02484b57bfb0c1d0e50e2e4
Daniel Johnson <dj125466@gmail.com>
2023-01-07 16:10:18 +0100

    First commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HeroSection, HeroLeft, HeroRight} from './HeroStyles';
+
+const Hero = () => {
+  return (
+    <HeroSection>
+        <HeroLeft>left</HeroLeft>
+        <HeroRight>HeroRight</HeroRight>
+    </HeroSection>
+     )
+}
+
+export default Hero
---
+++ src/components/Hero/HeroStyles.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const HeroSection = styled.div`
+    display: 'flex';
+    justify-content: 'space-between';
+`
