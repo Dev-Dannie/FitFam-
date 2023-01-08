004b6c45913b81e8d1639269e8cacdef53209d38
Daniel Johnson <dj125466@gmail.com>
2023-01-08 19:05:09 +0100

    hero updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { HeroSection, HeroLeft, HeroRight} from './HeroStyles';
+import { HeroSection, HeroLeft, HeroRight, BestAd, BestAdDiv,
+   BestAdSpan, HeroText, ThirdDiv, FiguresDiv, 
+   Figures, FiguresSpan1, FiguresSpan2} from './HeroStyles';
 
 const Hero = () => {
   return (
     <HeroSection>
         <HeroLeft>
             <Header />
+            <BestAd>
+              <BestAdDiv></BestAdDiv>
+              <BestAdSpan>The best fitness gym in town</BestAdSpan>
+            </BestAd>
+
+            <HeroText>
+              <div>
+                <span className='stroke-text'>Shape </span>
+                <span>Your</span>
+              </div>
+
+              <div>
+                <span>Ideal body</span>
+              </div>
+
+            <ThirdDiv>
+              <span>
+                We will help you to shape and build your ideal body goals.
+              </span>
+            </ThirdDiv>
+            </HeroText>
+
+            <Figures>
+              <FiguresDiv>
+                <FiguresSpan1>86+</FiguresSpan1>
+                <FiguresSpan2>expert coaches</FiguresSpan2>
+              </FiguresDiv>
+
+              <FiguresDiv>
+                <FiguresSpan1>630+</FiguresSpan1>
+                <FiguresSpan2>members joined</FiguresSpan2>
+              </FiguresDiv>
+
+              <FiguresDiv>
+                <FiguresSpan1>50+</FiguresSpan1>
+                <FiguresSpan2>fitness programs</FiguresSpan2>
+              </FiguresDiv>
+            </Figures>
         </HeroLeft>
         <HeroRight>HeroRight</HeroRight>
     </HeroSection>
--- src/components/Hero/HeroStyles.jsx
+++ src/components/Hero/HeroStyles.jsx
@@ -8,9 +8,79 @@ export const HeroSection = styled.div`
 export const HeroLeft = styled.div`
     padding: 2rem;
     padding-top: 1.5rem;
-    flex:3
+    flex:3;
+    display: flex;
+    gap:2rem;
+    flex-direction: column;
 `
 
 export const HeroRight = styled.div`
 flex:1
+`
+
+export const BestAd = styled.div`
+    background-color: #363d43;
+    margin-top: 4rem;
+    border-radius: 4rem;
+    padding: 20px 14px;
+    width: fit-content;
+    text-transform: uppercase;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    position: relative;
+`
+
+export const BestAdDiv = styled.div`
+    position: absolute;
+    background-color: var(--orange);
+    width: 5.4rem;
+    height:  80%;
+    left: 8px;
+    border-radius: 3rem;
+    z-index: 1;
+`
+
+export const BestAdSpan = styled.span`
+    z-index: 2;
+`
+
+export const HeroText = styled.div`
+display:flex;
+flex-direction: column;
+gap: 1.5rem;
+text-transform: uppercase;
+font-size: 4.5rem;
+font-weight: bold;
+color: white;
+text-overflow: inherit;
+`
+
+export const ThirdDiv = styled.div`
+font-size: 1rem;
+font-weight: 200;
+text-transform: none;
+letter-spacing: 1px;
+width: 80%
+`
+
+export const Figures = styled.div`
+display: flex;
+gap: 2rem;
+`
+
+export const FiguresDiv = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const FiguresSpan1 = styled.span`
+color: white;
+font-size: 2rem;
+`
+
+export const FiguresSpan2 = styled.span`
+color: var(--gray);
+text-transform: uppercase;
 `
