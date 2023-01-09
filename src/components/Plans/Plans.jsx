e8b8131a35b0d87af4b983affa36ad03c685b0ea
Daniel Johnson <dj125466@gmail.com>
2023-01-10 00:06:57 +0100

    plans section added

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {plansData} from '../../data/plansData';
+import { Feature, FeatureImg, Features, FeatureSpan, Plan, PlansContainer, PlansSection, } from './PlansStyles';
+import { ProgramsHeader} from '../Programs/ProgramsStyles'
+import whiteTick from '../../assets/whiteTick.png'
+
+const Plans = () => {
+  return (
+   <PlansContainer>
+        <ProgramsHeader>
+            <span className='stroke-text'>READY TO START</span>
+            <span>YOUR JOURNEY</span>
+            <span className='stroke-text'>NOW WITH US</span>
+        </ProgramsHeader>
+
+        {/* plans card */}
+
+        <PlansSection>
+            {plansData.map((plan, i) => (
+                <Plan className='plan'>
+                    {plan.icon}
+                    <span>{plan.name}</span>
+                    <span>{plan.price}</span>
+
+                    <Features>
+                        {plan.features.map((feature, i) => (
+                            <Feature>
+                                <FeatureImg src= {whiteTick} alt='' />
+                                <FeatureSpan key={i}>{feature}</FeatureSpan>
+                            </Feature>
+                        ))}
+                    </Features>
+
+                    <div className="">
+                        <span>See more benefits</span>
+                    </div>
+                    <button className='btn'>Join Now</button>
+                </Plan>
+            ))}
+        </PlansSection>
+   </PlansContainer>
+    )
+}
+
+export default Plans
---
+++ src/components/Plans/PlansStyles.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+
+export const PlansContainer = styled.div`
+padding:0 2rem;
+margin-top:-4rem;
+display:flex;
+flex-direction:column;
+gap:4rem;
+position:relative
+`
+
+export const PlansSection = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+gap:3rem;
+`
+
+export const Plan = styled.div`
+display:flex;
+flex-direction:column;
+background: var(--caloryCard);
+color:white;
+gap:2rem;
+padding:1.5rem;
+width:15rem
+`
+
+export const Features = styled.div`
+
+`
+export const Feature = styled.div`
+
+`
+export const FeatureImg = styled.img`
+
+`
+export const FeatureSpan = styled.span`
+
+`
+
