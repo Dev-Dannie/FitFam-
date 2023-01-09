05125d337ae342c523e46536a0f07912b96ba777
Daniel Johnson <dj125466@gmail.com>
2023-01-09 16:12:16 +0100

    Reasons Section added

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { LeftReasons, LeftReasonsImg, ReasonsSection, RightReasons } from './ReasonsStyles';
+import { DetailsDiv, DetailsImg, LeftReasons,  LeftReasonsImg1,  LeftReasonsImg2,  LeftReasonsImg3,  LeftReasonsImg4,  PartnersDiv,  PartnersImg,  ReasonsSection, RightReasons, RRDetails, RRDiv, RRSpan } from './ReasonsStyles';
 import image1 from '../../assets/image1.png';
 import image2 from '../../assets/image2.png';
 import image3 from '../../assets/image3.png';
@@ -13,14 +13,48 @@ const Reasons = () => {
   return (
     <ReasonsSection>
         <LeftReasons>
-            <LeftReasonsImg src={image1} alt=''/>        
-            <LeftReasonsImg src={image2} alt=''/>
-            <LeftReasonsImg src={image3} alt=''/>
-            <LeftReasonsImg src={image4} alt=''/>
+            <LeftReasonsImg1 src={image1} alt=''/>        
+            <LeftReasonsImg2 src={image2} alt=''/>
+            <LeftReasonsImg3 src={image3} alt=''/>
+            <LeftReasonsImg4 src={image4} alt=''/>
         </LeftReasons>
 
         <RightReasons>
-            Right Side
+            <RRSpan>some reasons</RRSpan>
+
+            <RRDiv className="">
+                <span className='stroke-text'>why</span>
+                <span> choose us?</span>
+            </RRDiv>
+
+            <RRDetails>
+                <DetailsDiv className="">
+                    <DetailsImg src={tick} alt="" />
+                    <span>OVER 150+ EXPERT COACHES</span>
+                </DetailsDiv>
+                <DetailsDiv className="">
+                    <DetailsImg src={tick} alt="" />
+                    <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+                </DetailsDiv>
+                <DetailsDiv className="">
+                    <DetailsImg src={tick} alt="" />
+                    <span>1 FREE PROGRAM FOR NEW MEMBERS</span>
+                </DetailsDiv>
+                <DetailsDiv className="">
+                    <DetailsImg src={tick} alt="" />
+                    <span>RELIABLE PARTNERS</span>
+                </DetailsDiv>
+            </RRDetails>
+
+            <span style={{color:'var(--gray)', fontWeight:'normal'}}>
+                OUR PARTNERS
+            </span>
+
+            <PartnersDiv>
+                <PartnersImg src={nb} alt=''/>
+                <PartnersImg src={adidas} alt=''/>
+                <PartnersImg src={nike} alt=''/>
+            </PartnersDiv>
         </RightReasons>
     </ReasonsSection>
   )
--- src/components/Reasons/ReasonsStyles.jsx
+++ src/components/Reasons/ReasonsStyles.jsx
@@ -13,10 +13,69 @@ grid-template-columns: repeat(3, 1fr);
 gap: 1rem;
 grid-auto-rows: 1fr;
 `
-export const LeftReasonsImg = styled.img`
-object-fit:cover
+export const LeftReasonsImg1 = styled.img`
+
+    width:12rem;
+    height:28rem;
+    grid-row: 1/3;
+`
+export const LeftReasonsImg2 = styled.img`
+    grid-column: 2/4;
+    width:auto;
+    height:16rem;
+`
+export const LeftReasonsImg3 = styled.img`
+    grid-column:2/3;
+    width:14rem;
+    grid-row:2;
+`
+export const LeftReasonsImg4 = styled.img`
+    grid-column:3/4;
+    grid-row:2;
+    width:10rem;
+    height:11.2rem;
 `
 
 export const RightReasons = styled.div`
+display: flex;
+flex-direction:column;
+gap:1rem;
+text-transform:uppercase;
 flex: 1 1;
+`
+
+export const RRSpan = styled.span`
+font-weight: bold;
+color:var(--orange)
+`
+export const RRDiv = styled.div`
+color:white;
+font-size:3rem;
+font-weight:bold;
+`
+
+export const RRDetails = styled.div`
+display:flex;
+flex-direction:column;
+gap:1rem;
+`
+
+export const DetailsDiv = styled.div`
+display:flex;
+font-size:1rem;
+gap:1rem;
+`
+
+export const DetailsImg = styled.img`
+width:2rem;
+height:2rem;
+`
+
+export const PartnersDiv = styled.div`
+display:flex;
+gap:1rem;
+`
+
+export const PartnersImg = styled.img`
+width:2.5rem
 `
